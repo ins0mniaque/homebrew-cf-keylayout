d9b28dc80cc7a8b60b362b7a4fd9fa46e728dee2
Jean-Philippe Leconte <ins0mniaque@gmail.com>
2024-10-31 06:49:14 -0400

    Fix formula

--- Casks/cf-keylayout.rb
+++ Casks/cf-keylayout.rb
@@ -3,11 +3,11 @@ cask "cf-keylayout" do
   sha256 :no_check
 
   homepage "https://github.com/ins0mniaque/cf-keylayout"
-  url "https://github.com/ins0mniaque/cf-keylayout/archive/master.zip"
+  url "https://github.com/ins0mniaque/cf-keylayout/archive/main.zip"
 
   name "Canadian French keyboard layout"
   desc "Canadian French keyboard layout for macOS"
 
-  artifact "cf-keylayout-master/Canadian French.keylayout", target: "#{ENV["HOME"]}/Library/Keyboard Layouts/Canadian French.keylayout"
-  artifact "cf-keylayout-master/Canadian French.icns", target: "#{ENV["HOME"]}/Library/Keyboard Layouts/Canadian French.icns"
+  artifact "cf-keylayout-main/Canadian French.keylayout", target: "#{ENV["HOME"]}/Library/Keyboard Layouts/Canadian French.keylayout"
+  artifact "cf-keylayout-main/Canadian French.icns", target: "#{ENV["HOME"]}/Library/Keyboard Layouts/Canadian French.icns"
 end
